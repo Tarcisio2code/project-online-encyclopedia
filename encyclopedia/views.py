04f71cc445fd8c062ece18500768ccb7a46c7262
Tarcisio Silva <76399323+Tarcisio2code@users.noreply.github.com>
2022-11-01 22:21:17 +0100

    Add markdown convert function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
+def markdown_converter(title):
+    source = util.get_entry(title)
+    markdowner = Markdown()
+    if source == None:
+        return None
+    else:
+        return markdowner.convert(source)
 
 def index(request):
-    entries = util.list_entries()
-    css_file = util.get_entry("CSS")
-    coffee = util.get_entry("coffee")
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
